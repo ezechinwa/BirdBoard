96a788d44f8fbc399347ee38d532837436570c8d
Chinwa Williams <chinwawilliams@gmail.com>
2019-05-28 00:02:03 +0100

    a_user_can_create_a_project() test passed

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        // $disableExceptionHandling  = $this->withot
+        $disableExceptionHandling = $this->withoutExceptionHandling();
+        $faker = $this->faker;
+        $attributes = [
+            'title' => $faker->sentence,
+            'description' => $faker->paragraph,
+        ];
+        $this->post('/projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+}
